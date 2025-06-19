1284d4a09001358d8c8567fb4545b263e86cc560
iptv <felixyong143@gmail.com>
2025-06-19 13:33:31 +0800

    Update ArtPlayerComponent.tsx

--- app/components/ArtPlayerComponent.tsx
+++ app/components/ArtPlayerComponent.tsx
@@ -1,24 +1,40 @@
-'use client';
-import { useEffect, useRef } from 'react';
 import Artplayer from 'artplayer';
 
-export default function ArtPlayerComponent({ url }) {
+export default function ArtPlayerComponent({ url }: { url: string }) {
   const artRef = useRef(null);
 
   useEffect(() => {
-    if (artRef.current && url) {
-      const art = new Artplayer({
-        container: artRef.current,
-        url,
-        isLive: true,
-        muted: true,
-        autoplay: true,
-        setting: true,
-        fullscreen: true,
-      });
-      return () => art.destroy();
-    }
+    const art = new Artplayer({
+      container: artRef.current,
+      url,
+      type: 'm3u8',
+      autoplay: true,
+      isLive: true,
+      muted: true,
+      setting: true,
+      fullscreen: true,
+      playsInline: true,
+      customType: {
+        m3u8: (video, url) => {
+          if (Hls.isSupported()) {
+            const hls = new Hls({
+              xhrSetup: function (xhr) {
+                xhr.withCredentials = false;
+              },
+            });
+            hls.loadSource(url);
+            hls.attachMedia(video);
+          } else {
+            video.src = url;
+          }
+        },
+      },
+    });
+
+    return () => {
+      art.destroy();
+    };
   }, [url]);
 
-  return <div ref={artRef} style={{ width: '100%', height: 480 }} />;
+  return <div ref={artRef} style={{ width: '100%', height: '60vh' }} />;
 }
